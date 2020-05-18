2a93a6076ad7d4dc19a48280e39600ab7b7fc414
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2020-05-18 16:59:39 -0400

    Update with_pos.js

--- with_pos.js
+++ with_pos.js
@@ -1,12 +1,23 @@
 //check if input contain a substring 'pos'
+/*
+if (pos==true){
+find "(" [return the index of "("]
+find ")" [return the index of ")"]
+return the string between "(" and ")"
+}
+
+*/
+
 function within(input) {
   var kw="POS"
   var str = String(input);
   var res = str.toUpperCase();
-  var n = res.includes(kw);
-  if (n == true) {
-  return "SWITCH";
-}
- 
-  return '0'
+  var boo = res.includes(kw);
+  if (boo==true){
+  var nf = str.indexOf("(");
+  var nt = str.indexOf(")");  
+  return str.slice(0, 5); 
+  }
+  return 0
+
 }
