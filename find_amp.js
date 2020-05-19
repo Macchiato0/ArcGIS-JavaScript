0591eccd5461d4a6e81a2c967baf4c12a9909e69
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2020-05-19 10:17:11 -0400

    Update find_amp.js

--- find_amp.js
+++ find_amp.js
@@ -5,14 +5,16 @@ return i
 */
 
 function find_amp(input) {
-  var kws=["200A","300A","500A","100A","600A","1000A","1200","1500A","1600A","2000A","3000A","4000A","5000A"]
+  var kws=["200A","300A","400A","633A","500A","100A","600A","700A","800A","1000A","1200A","1500A","1600A","2000A","3000A","4000A","5000A"]
   var str = String(input);
   var res = str.toUpperCase();
+  var words = res.split(" ");
   
   for (i = 0; i < kws.length; i++) { 
-  var boo = res.includes(kws[i]);
+  var boo = words.includes(kws[i]);
   if (boo==true){ 
        return kws[i];}
   }
   return 0
 }
+
